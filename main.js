15506f60be2a3998808d74fd7765070aa62d4451
Juan Avila <123758906+JuanDavidAvilaRaveloCampus@users.noreply.github.com>
2023-04-14 06:44:34 +0000

    test: :white_check_mark: Funtional
    Todo sirve, por medio de la clase
    
    test by: Juan Avila uwu

--- main.js
+++ main.js
@@ -1,64 +1,112 @@
-let tamaño_lapiz = document.querySelector(`[name = "dimension"]`);
-let color = document.querySelector(`[name = "color"]`);
-let marca = document.querySelector(`[name = "marca"]`);
-let borrador = document.querySelector(`[name = borrador]`)
-let material = document.querySelector(`[name = material]`)
+let input_color = document.querySelector(`[type='color']`);
+let input_rango = document.querySelector(`[type = 'range']`);
+let input_marca = document.querySelector(`[value='Mongol']`);
+let input_borrador = document.querySelector(`[value = 'true']`);
+let input_material = document.querySelector(`[value = 'Madera']`);
 
-let formulario = document.querySelector('#formulario').addEventListener('submit', (e)=>{
-    e.preventDefault();
-    let formulario = Object.fromEntries(new FormData (e.target));
+let data = [];
+class lapiz {
+    #marca
+    constructor({
+        color = 'amarillo',
+        dimension = 19.5,
+        borrador = true,
+        material = 'madera',
+        marca = 'mongol' }) {
+
+        this.color = color;
+        this.dimension = dimension;
+        this.material = material;
+        this.borrador = true,
+        this.#marca = marca;
+    };
+
+    getColor(){
+        return this.color;
+    }
+
+    getDimension(){
+        return this.dimension;
+    }
+
+    getMaterial(){
+        return this.material
+    }
+
+    getBorrador(){
+        return this.borrador;
+    };
+
+    getMarca() {
+        return this.#marca;
+    };
+
+    setColor(nuevo_color){
+        this.color = nuevo_color;
+    };
+
+    setDimension(nuevo_tamaño){
+        this.dimension = nuevo_tamaño;
+    };
+
+    setMarca(nueva_marca){
+        this.marca = nueva_marca;
+    }
+
+    setBorrador(new_borrador) {
+        this.borrador = new_borrador;
+    };
+
+    setMaterial(nuevo_material){
+        this.material = nuevo_material;
+    };
+
     
-    console.log(formulario['dimension']);
 
+}
+
+window.onload = ()=>{
+    input_color.setAttribute('value','#FFD700');
+    input_rango.val = 19.5;
+    input_marca.checked = true;
+    input_borrador.checked = true;
+    input_material.checked = true;
+
+    // console.log(color);
+};
+
+document.querySelector('#formulario').addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    let formulario = Object.fromEntries(new FormData(e.target));
+    let color = formulario.color;
+    let dimension = formulario.dimension;
+    let marca = formulario.marca;
+    let borrador = formulario.borrador;
+    let material = formulario.material;
+
+    let Lapiz = new lapiz({});
+    Lapiz.setColor({color : `${color}`});
+    Lapiz.setDimension({dimension : `${dimension}`});
+    Lapiz.setMarca({marca : `${marca}`});
+    Lapiz.setBorrador({borrador : `${borrador}`});
+    Lapiz.setMaterial({material : `${material}`});
+
+    data.unshift(color, dimension, marca, borrador, material);
+    // data.unshift(formulario.color)
+    document.querySelector('#tabla').insertAdjacentHTML('beforeend',`
+        <tr>
+            <th>${color}</th>
+            <th>${dimension}</th>
+            <th>${marca}</th>
+            <th>${borrador}r</th>
+            <th>${material}</th>
+        </tr>
+    `)
+    console.log(data);
 });
 
 
-window.onload = ()=>{
-    
-    document.querySelector(`[type='color']`).val = F7EB69;
-
-
-    document.querySelector(`[type = 'range']`).val = 19.5;
-    let marca = document.querySelector(`[value='Mongol']`);
-    marca.checked = true;
-
-    let borrador = document.querySelector(`[value = 'true']`);
-    borrador.checked = true;
-
-    let material = document.querySelector(`[value = 'Madera']`);
-    material.checked = true;
-};
 
 
 
-
-
-
-// class lapiz {
-//     #marca
-//     constructor({
-//         color = 'amarillo',
-//         dimension = 19.5,
-//         material = 'madera',
-//         marca = 'mongol' }) {
-
-//         this.color = color;
-//         this.dimension = dimension;
-//         this.material = material;
-//         this.#marca = marca;
-//     };
-
-//     setMarca(marca) {
-//         this.#marca = marca;
-//         return this.getMarca();
-//     };
-
-//     getMarca() {
-//         return this.#marca;
-//     }
-// }
-
-// let obj = new lapiz({});
-// obj.setMarca({marca : 'Norma'});
-
-// console.log(obj.getMarca());
